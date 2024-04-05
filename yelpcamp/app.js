1c0afc351b98570a08069a858dcf61e71c5ea9e7
Fulya Cimendere <fulyacoskun.87@gmail.com>
2024-04-05 14:01:36 +0200

    create new route

--- yelpcamp/app.js
+++ yelpcamp/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const Campground = require('./models/campground');
+const { reset } = require('nodemon');
 
 const mongoUrl = 'mongodb://127.0.0.1:27017/yelp-camp';
 
@@ -21,6 +22,8 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(express.urlencoded({extended:true}));
+
 app.get('/', (req,res)=> {
     res.render('home')
 })
@@ -31,16 +34,26 @@ app.get('/campgrounds', async (req,res)=> {
     res.render('campgrounds/index', {campgrounds});
 })
 
+//CRUD - CREATE | route for showing new form on page
+app.get('/campgrounds/new', (req,res) => {
+    res.render('campgrounds/new');
+})
+
+//CRUD - CREATE | route for collecting info from body of the post and save to the DB
+app.post('/campgrounds', async (req, res) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`)
+})
+
 // Campground details show route
 app.get('/campgrounds/:id', async(req,res) => {
     const campground = await Campground.findById(req.params.id);
     res.render('campgrounds/show', {campground});
 })
 
-//CRUD - CREATE | route for create new campgrounds
-app.get('/campgrounds/new', (req,res) => {
-    res.render('campgrounds/new');
-})
+
+
 
 //Server Listen
 app.listen(3000, () => {
