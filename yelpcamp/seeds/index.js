c5df3a5fc7cae85b43fbb2e01c6a4346109b9f2d
Fulya Cimendere <fulyacoskun.87@gmail.com>
2024-04-09 11:19:24 +0200

    add new value into the DB

--- yelpcamp/seeds/index.js
+++ yelpcamp/seeds/index.js
@@ -20,10 +20,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for(let i = 0 ; i < 50 ; i++){
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) +10;
         const camp = new Campground({
             location: `${cities[random1000].city} , ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
-            image: `https://unsplash.com/collections/483251`
+            image: `https://unsplash.com/collections/483251`,
+            description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum vitae ad excepturi. Quia, sunt minima dolore hic exercitationem ex. Consectetur similique tenetur ab officia repellat quasi reiciendis doloribus fuga quam.',
+            price: price
         })
         await camp.save();
     }
