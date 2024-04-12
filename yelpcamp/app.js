f054b32fa13f4a496b6de56c52a36592da8be49c
Fulya Cimendere <fulyacoskun.87@gmail.com>
2024-04-12 16:03:27 +0200

    basic error handling

--- yelpcamp/app.js
+++ yelpcamp/app.js
@@ -45,10 +45,14 @@ app.get('/campgrounds/new', (req,res) => {
 })
 
 //CRUD - CREATE | route for submitting form (new add form)
-app.post('/campgrounds', async (req, res) => {
-    const campground = new Campground(req.body.campground);
-    await campground.save();
-    res.redirect(`/campgrounds/${campground._id}`)
+app.post('/campgrounds', async (req, res,next) => {
+    try{
+        const campground = new Campground(req.body.campground);
+        await campground.save();
+        res.redirect(`/campgrounds/${campground._id}`)
+    } catch (e) {
+        next(e);
+    }
 })
 
 
@@ -76,6 +80,10 @@ app.delete('/campgrounds/:id', async (req,res)=> {
     const {id} = req.params;
     await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds');
+})
+
+app.use((err,req,res,next) => {
+    res.send('something went wrong')
 })
 
 //Server Listen
