87462c07a8529265f838f2952730de5c1d4f0849
Fulya Cimendere <fulyacoskun.87@gmail.com>
2024-04-23 10:52:32 +0200

    add reviews routing

--- yelpcamp/app.js
+++ yelpcamp/app.js
@@ -6,6 +6,7 @@ const ExpressError = require('./utils/ExpressError');
 const {campgroundSchema} = require('./schemas.js');
 const catchAsync = require('./utils/catchAsync');
 const Campground = require('./models/campground');
+const Review = require('./models/review')
 const { reset } = require('nodemon');
 const { request } = require('http');
 const methodOverride = require('method-override');
@@ -95,6 +96,16 @@ app.delete('/campgrounds/:id', catchAsync(async (req,res)=> {
     const {id} = req.params;
     await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds');
+}))
+
+app.post('/campgrounds/:id/reviews', catchAsync(async (req,res) => {
+    const campground = await Campground.findById(req.params.id);
+    const review = new Review(req.body.review);
+    campground.reviews.push(review);
+    await review.save();
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+    
 }))
 
 app.all('*', (req,res,next) => {
