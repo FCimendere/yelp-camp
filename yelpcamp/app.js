989a399373c26008582d23650a5a661c676f2911
Fulya Cimendere <fulyacoskun.87@gmail.com>
2024-04-26 10:42:41 +0200

    add session and flash

--- yelpcamp/app.js
+++ yelpcamp/app.js
@@ -2,10 +2,13 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
+const session = require('express-session');
+const flash = require('connect-flash');
 const ExpressError = require('./utils/ExpressError'); 
 const { reset } = require('nodemon');
 const { request } = require('http');
 const methodOverride = require('method-override');
+
 
 
 const campgrounds = require('./routes/campgrounds');
@@ -34,10 +37,30 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({extended:true}));
 app.use(methodOverride('_method'));
 
+app.use((req,res,next) => {
+    res.locals.success = req.flash('success');
+    next();
+})
+
 //routes 
 app.use('/campgrounds', campgrounds)
 app.use('/campgrounds/:id/reviews', reviews)
 app.use(express.static(path.join(__dirname,'public')))
+
+//session configuration
+const sessionConfig = {
+    secret: 'SimpleSecretKey',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000*60*60*24*7,
+        maxAge: 1000*60*60*24*7
+    }
+}
+app.use(session(sessionConfig))
+app.use(flash())
+
 
 app.get('/', (req,res)=> {
     res.render('home')
