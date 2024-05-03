761224f1e690cd440aae0528afcb20f4a8bafe17
Fulya Cimendere <fulyacoskun.87@gmail.com>
2024-05-03 15:23:20 +0200

    create users model

--- yelpcamp/models/user.js
+++ yelpcamp/models/user.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 const passportLocalMongoose = require('passport-local-mongoose');
-const {Schema} = mongoose;
 
-const UserSchema = new Schema ({
-    email : {
-        type:String, 
-        required:true,
+const UserSchema = new Schema({
+    email: {
+        type: String,
+        required: true,
         unique: true
     }
-})
+});
 
 UserSchema.plugin(passportLocalMongoose);
 
