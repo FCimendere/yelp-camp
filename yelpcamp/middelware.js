a3f6d825a9853bfcf237c3f9bd6964276c64a9f6
Fulya Cimendere <fulyacoskun.87@gmail.com>
2024-05-06 15:18:40 +0200

    update returnto Behavior

--- yelpcamp/middelware.js
+++ yelpcamp/middelware.js
@@ -1,6 +1,16 @@
 
-module.exports.isLoggedIn = (req,res,next) => {
-    if(!req.isAuthenticated()){
+//..storing the user current route before login
+module.exports.storeReturnTo = (req, res, next) => {
+    if (req.session.returnTo) {
+        res.locals.returnTo = req.session.returnTo;
+    }
+    next();
+}
+
+//..control the user authentication to log in
+module.exports.isLoggedIn = (req, res, next) => {
+    if (!req.isAuthenticated()) {
+        req.session.returnTo = req.originalUrl; 
         req.flash('error', 'You must be signed in first!');
         return res.redirect('/login');
     }
