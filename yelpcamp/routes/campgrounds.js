91cc29763d3465326a9ce38966032ee0e72d76ed
Fulya Cimendere <fulyacoskun.87@gmail.com>
2024-05-07 16:00:18 +0200

    refactoring to campgrounds controller

--- yelpcamp/routes/campgrounds.js
+++ yelpcamp/routes/campgrounds.js
@@ -5,75 +5,30 @@ const ExpressError = require('../utils/ExpressError');
 const Campground = require('../models/campground');
 const {campgroundSchema} = require('../schemas.js');
 const {isLoggedIn, isAuthor,validateCampground} = require('../middelware.js');
+//controllers
+const campgrounds = require('../controllers/campgrounds');
 
+//******CAMPGROUND ROUTES******/
 
 //CRUD - READ | route for all names of the campgrounds
-router.get('/', async (req,res)=> {
-    const campgrounds = await Campground.find({});
-    res.render('campgrounds/index', {campgrounds});
-})
+router.get('/', catchAsync(campgrounds.index));
 
 //CRUD - CREATE | route for showing form (new add form)
-router.get('/new', isLoggedIn, (req,res) => {
-    res.render('campgrounds/new');
-})
+router.get('/new', isLoggedIn, campgrounds.renderNewForm);
 
 //CRUD - CREATE | route for submitting form (new add form)
-router.post('/', isLoggedIn, validateCampground, catchAsync(async (req, res,next) => {
-    // if(!req.body.campground) throw new ExpressError('Invalid Campground Data',400)
-    const campground = new Campground(req.body.campground);
-    campground.author = req.user._id;
-    await campground.save();
-    req.flash('success', 'Successfully made a new campground!');
-    res.redirect(`/campgrounds/${campground._id}`)
-}))
-
+router.post('/', isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
 
 // Campground details show route
-router.get('/:id', catchAsync(async(req,res) => {
-    const campground = await Campground.findById(req.params.id).populate({
-        path:'reviews',
-        // populate of the review's author - not campground user-
-        populate: {
-            path:'author'
-        }
-    // populate of the campground's author-
-    }).populate('author');
-    // first way to show flash onto screen
-    //res.render('campgrounds/show', {campground, msg: req.flash('success')});
-    //better way to show flash onto screen - with middelware
-    if (!campground) {
-        req.flash('error', 'Cannot find that campground!');
-        return res.redirect('/campgrounds');
-    }
-    res.render('campgrounds/show', {campground});
-}))
+router.get('/:id', catchAsync(campgrounds.showCampground));
 
 //CRUD - UPDATE/EDIT | route for showing form (edit form)
-router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(async (req,res) => {
-    const { id } = req.params;
-    const campground = await Campground.findById(id)
-    if (!campground) {
-        req.flash('error', 'Cannot find that campground!');
-        return res.redirect('/campgrounds');
-    }
-    res.render('campgrounds/edit', { campground });
-}))
+router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditForm));
 
 //CRUD - UPDATE/EDIT | route for submitting form (edit form)
-router.put('/:id', isLoggedIn, isAuthor, validateCampground, catchAsync(async (req,res) => {
-    const { id } = req.params;
-    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
-    req.flash('success', 'Successfully updated campground!');
-    res.redirect(`/campgrounds/${campground._id}`);
-}))
+router.put('/:id', isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground));
 
 //CRUD - DELETE | delete a campground from the DB
-router.delete('/:id', isLoggedIn, isAuthor, catchAsync(async (req,res)=> {
-    const {id} = req.params;
-    await Campground.findByIdAndDelete(id);
-    req.flash('success', 'Successfully deleted campground')
-    res.redirect('/campgrounds');
-}))
+router.delete('/:id', isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
 
 module.exports = router;
