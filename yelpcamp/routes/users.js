e0d156ee524f927f9f96609f328368f38dd073e5
Fulya Cimendere <fulyacoskun.87@gmail.com>
2024-05-07 16:41:46 +0200

    refactoring user register/login routes/controls

--- yelpcamp/routes/users.js
+++ yelpcamp/routes/users.js
@@ -4,50 +4,24 @@ const catchAsync = require('../utils/catchAsync');
 const passport = require('passport');
 const User = require('../models/user');
 const { storeReturnTo } = require('../middelware');
+const users = require('../controllers/users');
 
-// user registration routing
-router.get('/register', (req,res) => {
-    res.render('users/register');
-})
+// user register form render route
+router.get('/register', users.renderRegister);
 
-router.post('/register', catchAsync(async(req,res) => {
-    try {
-        const {email, password, username} = req.body;
-        const user = new User({email,username});
-        const registeredUser = await User.register(user,password);
-        //after register, using user info to login automatically
-        req.login(registeredUser, err => {
-            if(err) return next(err);
-            req.flash('success', 'Welcome to YelpCamp');
-            res.redirect('/campgrounds');
-        })   
-    } catch (e){
-        req.flash('error', e.message);
-        res.redirect('/register');
-    }
-}))
+// user register form submit route
+router.post('/register', catchAsync(users.register));
 
-// user loging in routing
-router.get('/login', (req,res) => {
-    res.render('users/login');
-})
+// user login form render route
+router.get('/login', users.renderLogin);
 
-router.post('/login', storeReturnTo, passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), async(req,res) => {
-    req.flash('success','Welcome Back!');
-    //redirect user the page bafore logged in or campgrounds
-    const redirectUrl = res.locals.returnTo || '/campgrounds'; 
-    res.redirect(redirectUrl);
-})
+//user login form submit and authentication control route
+router.post('/login', 
+storeReturnTo, 
+passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), 
+users.login);
 
-// user logout routing
-router.get('/logout', (req, res, next) => {
-    req.logout(function (err) {
-        if (err) {
-            return next(err);
-        }
-        req.flash('success', 'Goodbye!');
-        res.redirect('/campgrounds');
-    });
-});
+// user logout route
+router.get('/logout', users.logout);
 
 module.exports = router;
