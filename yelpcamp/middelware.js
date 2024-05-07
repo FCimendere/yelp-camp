5ff75dd4fa0b3ce30d5602ea63a5efd4872cc8aa
Fulya Cimendere <fulyacoskun.87@gmail.com>
2024-05-07 13:59:55 +0200

    authorization middelware

--- yelpcamp/middelware.js
+++ yelpcamp/middelware.js
@@ -1,3 +1,7 @@
+const { campgroundSchema, reviewSchema } = require('./schemas.js');
+const ExpressError = require('./utils/ExpressError');
+const Campground = require('./models/campground');
+const Review = require('./models/review');
 
 //..storing the user current route before login
 module.exports.storeReturnTo = (req, res, next) => {
@@ -13,6 +17,16 @@ module.exports.isLoggedIn = (req, res, next) => {
         req.session.returnTo = req.originalUrl; 
         req.flash('error', 'You must be signed in first!');
         return res.redirect('/login');
+    }
+    next();
+}
+
+module.exports.isAuthor = async(req,res,next) => {
+    const {id} = req.params;
+    const campground = await Campground.findById(id);
+    if(!campground.author.equals(req.user._id)){
+        req.flash('error', 'You do not have permission!');
+        return res.redirect(`/campgrounds/${id}`);
     }
     next();
 }
