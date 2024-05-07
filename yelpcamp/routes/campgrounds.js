3d910bb6b843c908fe9f43fa925704ad422585d6
Fulya Cimendere <fulyacoskun.87@gmail.com>
2024-05-07 16:03:54 +0200

    update campgrounds.route

--- yelpcamp/routes/campgrounds.js
+++ yelpcamp/routes/campgrounds.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const catchAsync = require('../utils/catchAsync');
-const ExpressError = require('../utils/ExpressError'); 
-const Campground = require('../models/campground');
-const {campgroundSchema} = require('../schemas.js');
 const {isLoggedIn, isAuthor,validateCampground} = require('../middelware.js');
 //controllers
 const campgrounds = require('../controllers/campgrounds');
