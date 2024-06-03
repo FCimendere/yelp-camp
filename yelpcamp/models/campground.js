3c8b382c59c7c03e1cc80999b76aca7b1182dd3d
Fulya Cimendere <fulyacoskun.87@gmail.com>
2024-06-03 15:19:06 +0200

    updating delete images part for the edit page

--- yelpcamp/models/campground.js
+++ yelpcamp/models/campground.js
@@ -4,12 +4,22 @@ const mongoose = require('mongoose');
 const Review = require('./review');
 const Schema = mongoose.Schema;
 
-const CampgroundSchema = new Schema ({
-    title: String,
-    images: [{
+// setting up a virtual property
+// url: 'https://res.cloudinary.com/dsbhzm1ku/image/upload/w_300/v1717416750/yelpcamp/qi1zkdvlnv6omkarfep8.jpg'
+
+const ImageSchema = new Schema({
         url: String,
         filename: String
-    }],
+    });
+
+ImageSchema.virtual('thumbnail').get(function () {
+    //this could be regular expressions
+    return this.url.replace('/upload', '/upload/w_200');
+});
+
+const CampgroundSchema = new Schema ({
+    title: String,
+    images: [ImageSchema],
     price: Number,
     description: String,
     location: String,
