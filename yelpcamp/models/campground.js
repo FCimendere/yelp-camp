81ec194a497e6bac0f6a30c791df280bd8f75c77
Fulya Cimendere <fulyacoskun.87@gmail.com>
2024-06-04 16:09:36 +0200

    adding cluster map and update its features

--- yelpcamp/models/campground.js
+++ yelpcamp/models/campground.js
@@ -17,6 +17,8 @@ ImageSchema.virtual('thumbnail').get(function () {
     return this.url.replace('/upload', '/upload/w_200');
 });
 
+const opts = {toJSON: { virtuals: true}};
+
 const CampgroundSchema = new Schema ({
     title: String,
     images: [ImageSchema],
@@ -45,6 +47,12 @@ const CampgroundSchema = new Schema ({
             ref:'Review'
         }
     ]
+},opts);
+
+CampgroundSchema.virtual('properties.popUpMarkup').get(function () {
+    //this could be regular expressions
+    return `<strong><a href="/campgrounds/${this._id}">${this.title}</a></strong>
+    <p>${this.description.substring(0, 20)}...</p>`
 });
 
 //deleting campground with all related reviews 
--- yelpcamp/seeds/index.js
+++ yelpcamp/seeds/index.js
@@ -18,7 +18,7 @@ const sample =  array => array[Math.floor(Math.random() * array.length)];
 //create 50 random places name
 const seedDB = async () => {
     await Campground.deleteMany({});
-    for(let i = 0 ; i < 50 ; i++){
+    for(let i = 0 ; i < 200 ; i++){
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) +10;
         const camp = new Campground({
@@ -29,7 +29,10 @@ const seedDB = async () => {
             price,
             geometry: { 
                 type: 'Point', 
-                coordinates: [ -75.97661, 36.849037 ] 
+                coordinates: [ 
+                    cities[random1000].longitude, 
+                    cities[random1000].latitude
+                ] 
                 },
             images: [
                 {
@@ -52,3 +55,4 @@ const seedDB = async () => {
 seedDB().then(() => {
     mongoose.connection.close();
 })
+
