d70fd26cdf1188cad18de4b9c894ed5a484131e3
Fulya Cimendere <fulyacoskun.87@gmail.com>
2024-06-05 11:55:53 +0200

    minor session cookie update

--- yelpcamp/app.js
+++ yelpcamp/app.js
@@ -49,11 +49,13 @@ app.use(mongoSanitize());
 
 //session configuration
 const sessionConfig = {
+    name: 'session',
     secret: 'SimpleSecretKey',
     resave: false,
     saveUninitialized: true,
     cookie: {
         httpOnly: true,
+        // secure: true,
         expires: Date.now() + 1000*60*60*24*7,
         maxAge: 1000*60*60*24*7
     }
